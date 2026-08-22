72c977a9fda4bae37defec7adeb5dab4b6053a06
CemXokenc <mohikancem@gmail.com>
2026-08-22 23:34:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        command_list = command.split(" ")
+
+        if command_list[0] != "cp" or command_list[1] == command_list[2]:
+            return
+
+        try:
+            with (
+                open(command_list[1], "r") as file_in,
+                open(command_list[2], "w") as file_out
+            ):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            return
+    except IndexError:
+        return
